f10701c0ad31f5efd5d0186e7d01368b9736c24e
thanh long <thanhlong274@hotmail.com>
2026-04-27 13:38:32 +0700

    chore(app): bootstrap FastAPI project with core configuration
    - Initialize FastAPI application with Granian as the Rust-based ASGI server.
    - Add pydantic-settings with python-dotenv for robust environment management.
    
    - implement CORS handling
    
    - integrate Sentry SDK for error monitoring (production only)
    
    - define API prefix and basic app configuration
    
    - add python-version: 3.14 for Runner instead of default version
    
    - using --frozen flag to use exactly version inside uv.lock

---
+++ app/core/config.py
@@ -0,0 +1,49 @@
+from pathlib import Path
+from typing import Annotated, Any, Literal
+
+from pydantic import AnyUrl, BeforeValidator, HttpUrl, computed_field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+def parse_cors(raw_value: Any) -> list[str] | str:
+    """
+    Parse CORS origins from .env
+
+    Example:
+        "http://localhost:3000,http://localhost:5173"
+        -> ["http://localhost:3000", "http://localhost:5173"]
+    """
+    if isinstance(raw_value, str) and not raw_value.startswith("["):
+        return [i.strip() for i in raw_value.split(",") if i.strip()]
+    elif isinstance(raw_value, list | str):
+        return raw_value
+    raise ValueError("Invalid CORS origins format: {0}", raw_value)
+
+
+BASE_DIR = Path(__file__).resolve().parents[2]
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=BASE_DIR / ".env", env_ignore_empty=True, extra="ignore"
+    )
+    API_PREFIX: str = "/api"
+    PROJECT_NAME: str
+    ENVIRONMENT: Literal["dev", "prod"] = "prod"
+
+    # Sentry
+    SENTRY_DSN: HttpUrl | None = None
+
+    # CORS
+    FRONTEND_HOST: str = "http://localhost:3000"
+    BACKEND_CORS_ORIGINS: Annotated[list[AnyUrl], BeforeValidator(parse_cors)] = []
+
+    @computed_field
+    @property
+    def all_cors_origins(self) -> list[str]:
+        return [str(origin).rstrip("/") for origin in self.BACKEND_CORS_ORIGINS] + [
+            self.FRONTEND_HOST
+        ]
+
+
+settings = Settings()  # type: ignore
--- app/main.py
+++ app/main.py
@@ -1,2 +1,43 @@
-def hello():
-    pass
+import sentry_sdk
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.api.main import api_router
+from app.core.config import settings
+
+if settings.SENTRY_DSN and settings.ENVIRONMENT == "prod":
+    sentry_sdk.init(
+        dsn=str(settings.SENTRY_DSN),
+        # Add data like request headers and IP for users,
+        # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
+        send_default_pii=False,
+        # Enable sending log info/debug/error to Sentry
+        enable_logs=True,
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for tracing.
+        traces_sample_rate=0.1,
+        # Set profile_session_sample_rate to 1.0 to profile 100%
+        # of profile sessions.
+        profile_session_sample_rate=0.1,
+        # Set profile_lifecycle to "trace" to automatically
+        # run the profiler on when there is an active transaction
+        profile_lifecycle="trace",
+    )
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    openapi_url=f"{settings.API_PREFIX}/openapi.json",
+    generate_unique_id_function=lambda route: (
+        f"{route.tags[0] if route.tags else 'default'}-{route.name}"
+    ),
+)
+
+# Set up CORS middleware
+if settings.all_cors_origins:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=settings.all_cors_origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+app.include_router(api_router, prefix=settings.API_PREFIX)
