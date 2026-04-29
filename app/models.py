01613ac3e7a01bbde12864077c746750802ba3c1
thanh long <thanhlong274@hotmail.com>
2026-04-29 08:14:59 +0700

    chore(infra): Update table name for user table

--- app/models.py
+++ app/models.py
@@ -55,6 +55,7 @@ class UpdatePassword(SQLModel):
 
 # Database model, database table inferred from class name
 class User(UserBase, table=True):
+    __tablename__ = "users"
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     hashed_password: str
     created_at: datetime | None = Field(
